6afe9a63d5b0acef705e0b9e2fa01790531737b1
Peter Sturm <sturm@uni-trier.de>
2022-08-09 12:59:31 +0200

    Correct message content sent

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,10 @@
     https://github.com/espressif/esp-idf/blob/master/examples/protocols/sockets/udp_multicast/main/udp_multicast_example_main.c
 */
 
+#define DEVICE_NAME "Nemo-01"
+#define POLL_INTERVALL 60    // seconds
+#define BACKLOG_SIZE 12
+#define MAX_MESSAGE_SIZE 1024
 
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
@@ -21,6 +25,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "nvs_flash.h"
+#include "esp_timer.h"
 
 static const char *TAG = "esp32_dht22";
 
@@ -36,6 +41,16 @@ extern "C" {
     void app_main(void);
     #include "DHT22.h"
 }
+struct Samples {
+    unsigned int seq_number;
+    int64_t current;
+    float current_temperature;
+    float backlog_t[BACKLOG_SIZE];
+    float current_humidity;
+    float backlog_h[BACKLOG_SIZE];
+};
+
+static Samples samples;
 
 // ********************************************************************************
 // Main
@@ -45,8 +60,18 @@ void DHT_task(void *pvParameter)
 {
 }
 
-void app_main()
-{
+void app_main() {
+    int64_t start_time = esp_timer_get_time();
+
+    for (int i=0; i<BACKLOG_SIZE; i++) {
+        samples.backlog_t[i] = -100.0;
+        samples.backlog_h[i] = -100.0;
+    }
+    samples.current_temperature = -100.0;
+    samples.current_humidity = -100.0;
+    samples.current = 0;
+    samples.seq_number = 0;
+
     esp_event_loop_create_default();
 
     //Initialize NVS
@@ -67,24 +92,53 @@ void app_main()
         return;
     }
 
-
 	vTaskDelay( 1000 / portTICK_RATE_MS );
 	setDHTgpio(27);
 
-    int l = 0;
-	while(1) {
+    char message[MAX_MESSAGE_SIZE];
+    while(1) {
 		int ret = readDHT();		
 		errorHandler(ret);
 
-		printf( "%4d: Temperature %.1f C, Humidity %.1f\n", l++, getTemperature(), getHumidity() );
-		
-        const char sendfmt[] = "{ \"temperature\" : %.1f, \"humidity\" : %.1f }";
-        char sendbuf[48];
-        int len = snprintf(sendbuf, sizeof(sendbuf), sendfmt, getTemperature(), getHumidity());
-        if (len > sizeof(sendbuf)) {
-            ESP_LOGE(TAG, "Overflowed multicast sendfmt buffer!!");
-            break;
+        samples.current = (esp_timer_get_time() - start_time) / 1000;
+        for (int i=BACKLOG_SIZE-1;i>0; i--) {
+            samples.backlog_t[i] = samples.backlog_t[i-1];
+            samples.backlog_h[i] = samples.backlog_h[i-1];
         }
+        samples.backlog_t[0] = samples.current_temperature;
+        samples.backlog_h[0] = samples.current_humidity;
+        samples.current_temperature = getTemperature();
+        samples.current_humidity = getHumidity();
+        samples.seq_number++;
+
+        char *m_ptr = message;
+        int m_len_remain = MAX_MESSAGE_SIZE;
+        
+        const char fmt_head[] = " { \"device\":\"%s\", \"sequence\":%d, \"current\":%lld, \"current_t\":%.1f, \"current_h\":%.1f, \"backlog_t\":[";
+        int len = snprintf(m_ptr,m_len_remain,fmt_head,DEVICE_NAME,samples.seq_number,samples.current,samples.current_temperature,samples.current_humidity);
+        const char fmt_v[] = "%.1f,";
+        for (int i=0;i<BACKLOG_SIZE;i++) {
+            m_ptr += len;
+            m_len_remain -= len;
+            len = snprintf(m_ptr,m_len_remain,fmt_v,samples.backlog_t[i]);
+        }
+        m_ptr += len-1;
+        m_len_remain -= len-1;
+        const char fmt_m[] = "], \"backlog_h\":[";
+        len = snprintf(m_ptr,m_len_remain,fmt_m);
+        for (int i=0;i<BACKLOG_SIZE;i++) {
+            m_ptr += len;
+            m_len_remain -= len;
+            len = snprintf(m_ptr,m_len_remain,fmt_v,samples.backlog_h[i]);
+        }
+        m_ptr += len-1;
+        m_len_remain -= len-1;
+        const char fmt_tail[] = "] }";
+        len = snprintf(m_ptr,m_len_remain,fmt_tail);
+        m_ptr += len;
+        m_len_remain -= len;
+
+
         bool connected = false;
         while (!connected) {
             Wifi::state_e wifi_state = wifi.GetState();
@@ -104,11 +158,12 @@ void app_main()
                     ESP_LOGI(TAG,"Wifi state is %s - not yet dealing with it",Wifi::GetStateDescription(wifi_state));
             }
         }
-        ms.send(sendbuf,len);
+        printf("%s\n",message);
+        ms.send(message,MAX_MESSAGE_SIZE-m_len_remain);
 
 		// -- wait at least 2 sec before reading again ------------
 		// The interval of whole process must be beyond 2 seconds !! 
-		vTaskDelay( 5000 / portTICK_RATE_MS );
+		vTaskDelay( POLL_INTERVALL * 1000 / portTICK_RATE_MS );
 	}
 
 	// xTaskCreate( &DHT_task, "DHT_task", 2048, NULL, 5, NULL );
